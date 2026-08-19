f13cebba4b8c5ba822b7c2e4eb992210c95ae93b
iuukai <790331286@163.com>
2026-08-19 20:32:33 +0800

    chore(agent): 移除express路由导出并添加对应类型依赖
    首先删除了index.ts中对外暴露的express子路由代码，同时在agent包的package.json中新增了@types/express类型依赖包

--- packages/agent/index.ts
+++ packages/agent/index.ts
@@ -3,5 +3,3 @@ export { runExamAgent } from './core/agentLoop'
 export { chatCompletion } from './llmClient'
 // 全部类型导出
 export * from './types'
-// express子路由
-export { default as agentRouter } from './agent.router'
